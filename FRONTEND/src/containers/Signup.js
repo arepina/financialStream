35049ff88b7f5668b442a5cb0c9682583aaa3c94
Bokun Wei <bokun.w@gmail.com>
2019-08-12 15:36:38 +0100

    added post request for sign ups

--- FRONTEND/src/containers/Signup.js
+++ FRONTEND/src/containers/Signup.js
@@ -55,6 +55,25 @@ export default class Signup extends Component {
   handleSubmit = async event => {
     event.preventDefault();
 
+    let url = 'TO DO'  ;// put flask link here as a string!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!TO DO!!!!!!!!!!!!!!!!!!!!!
+
+    let data =  new FormData();
+    data.append("email", this.state.email);
+    data.append("password", this.state.password);
+    data.append("user type", this.state.rSelected);
+    fetch(url, {method: "POST", body: data, mode: "cors"}) 
+       //.then(checkStatus)
+       .then(res => res.json())
+        .then(function(res) {
+            console.log(res)
+        })
+       .catch(function(error) {
+           alert(error)
+       });
+
+
+
+
     this.setState({ isLoading: true });
 
     this.setState({ newUser: "test" });
