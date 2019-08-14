566668db66280c717447b66eeae9f04009e98d34
Client <team20grads@gmail.com>
2019-08-14 17:14:30 +0200

    log added

---
+++ dbBack/test_Database.py
@@ -0,0 +1,18 @@
+from Database import Database
+
+class Test_Database:
+
+    def test_add_stream_data(self):
+        instrumentName = "Galactia"
+        cpty = "Lewis"
+        price = 12345
+        type = "S"
+        quantity = 2
+        time = "11-Aug-2019 (12:07:07.042573)"
+        result = Database.add_stream_data(self, instrumentName, cpty, price, type, quantity, time)
+
+        print(result)
+
+
+
+
