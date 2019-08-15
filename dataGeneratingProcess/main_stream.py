b76dbbe7b234461ba40b1559168b95d794e9923e
team20grads@gmail.com <team20grads@gmail.com>
2019-08-15 14:51:02 +0100

    fixes

--- dataGeneratingProcess/main_stream.py
+++ dataGeneratingProcess/main_stream.py
@@ -28,14 +28,14 @@ def getstream():
             print(e)
             continue
 
-@app.route('/getStreamData')
-def getstreamfrontend():
-    def data_stream():
-            stream = RandomDealData()
-            instrumentList = stream.createInstrumentList()
-            while True:
-                yield 'data:{}\n\n'.format(stream.createRandomData(instrumentList))
-    return Response(data_stream(), mimetype="text/event-stream")
+# @app.route('/getStreamData')
+# def getstreamfrontend():
+#     def data_stream():
+#             stream = RandomDealData()
+#             instrumentList = stream.createInstrumentList()
+#             while True:
+#                 yield 'data:{}\n\n'.format(stream.createRandomData(instrumentList))
+#     return Response(data_stream(), mimetype="text/event-stream")
 
 def bootapp():
     app.run(port=8080, threaded=True, host=(endpoint.HOST), debug=True)
