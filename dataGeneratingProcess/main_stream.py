c4163c5fa8466275868a6c7ffd911a96da711a87
Soumya Singh <soumyasingh1@yahoo.com>
2019-08-15 14:37:02 +0100

    Changed data stream to SSE

--- dataGeneratingProcess/main_stream.py
+++ dataGeneratingProcess/main_stream.py
@@ -10,32 +10,31 @@ app = Flask(__name__)
 CORS(app)
 
 
+# @app.route('/')
+# def getstream():
+#     def data_stream():
+#         stream = RandomDealData()
+#         instrumentList = stream.createInstrumentList()
+#         while True:
+#             yield stream.createRandomData(instrumentList)
+
+#     for stream in data_stream():
+#         print(stream)
+#         try:
+#             logging.info(stream)
+#             requests.post(endpoint.POST_TO, json=stream)  # ERROR
+#         except Exception as e:
+#             logging.error("Error: ", str(e))
+#             print(e)
+#             continue
+
 @app.route('/')
-def getstream():
-    def data_stream():
-        stream = RandomDealData()
-        instrumentList = stream.createInstrumentList()
-        while True:
-            yield stream.createRandomData(instrumentList)
-
-    for stream in data_stream():
-        print(stream)
-        try:
-            logging.info(stream)
-            requests.post(endpoint.POST_TO, json=stream)  # ERROR
-        except Exception as e:
-            logging.error("Error: ", str(e))
-            print(e)
-            continue
-
-@app.route('/getStreamData')
 def getstreamfrontend():
-     def data_stream():
+    def data_stream():
             stream = RandomDealData()
-        instrumentList = stream.createInstrumentList()
-        while True:
-            yield 'data:{}\n\n'.format(stream.createRandomData(instrumentList))
-
+            instrumentList = stream.createInstrumentList()
+            while True:
+                yield 'data:{}\n\n'.format(stream.createRandomData(instrumentList))
     return Response(data_stream(), mimetype="text/event-stream")
 
 def bootapp():
