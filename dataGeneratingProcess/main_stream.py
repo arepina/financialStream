9e3ceaaa37dbe64010a8d1a635650688a210561d
Soumya Singh <soumyasingh1@yahoo.com>
2019-08-15 14:41:47 +0100

    created 2 functions for streaming data

--- dataGeneratingProcess/main_stream.py
+++ dataGeneratingProcess/main_stream.py
@@ -10,25 +10,25 @@ app = Flask(__name__)
 CORS(app)
 
 
-# @app.route('/')
-# def getstream():
-#     def data_stream():
-#         stream = RandomDealData()
-#         instrumentList = stream.createInstrumentList()
-#         while True:
-#             yield stream.createRandomData(instrumentList)
-
-#     for stream in data_stream():
-#         print(stream)
-#         try:
-#             logging.info(stream)
-#             requests.post(endpoint.POST_TO, json=stream)  # ERROR
-#         except Exception as e:
-#             logging.error("Error: ", str(e))
-#             print(e)
-#             continue
-
 @app.route('/')
+def getstream():
+    def data_stream():
+        stream = RandomDealData()
+        instrumentList = stream.createInstrumentList()
+        while True:
+            yield stream.createRandomData(instrumentList)
+
+    for stream in data_stream():
+        print(stream)
+        try:
+            logging.info(stream)
+            requests.post(endpoint.POST_TO, json=stream)  # ERROR
+        except Exception as e:
+            logging.error("Error: ", str(e))
+            print(e)
+            continue
+
+@app.route('/getStreamData')
 def getstreamfrontend():
     def data_stream():
             stream = RandomDealData()
