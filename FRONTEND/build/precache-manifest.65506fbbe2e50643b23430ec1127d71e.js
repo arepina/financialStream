7feb9efe866ac3907dad05a021bc687976a35342
Soumya Singh <soumyasingh1@yahoo.com>
2019-08-15 15:30:24 +0100

    update build file

---
+++ FRONTEND/build/precache-manifest.65506fbbe2e50643b23430ec1127d71e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c633529df37d4dc0e8596535a85128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c410a77262c3623263",
+    "url": "/static/css/2.8b5d52c6.chunk.css"
+  },
+  {
+    "revision": "70d1519c9c754334e016",
+    "url": "/static/css/main.2997cc70.chunk.css"
+  },
+  {
+    "revision": "01c410a77262c3623263",
+    "url": "/static/js/2.031e5987.chunk.js"
+  },
+  {
+    "revision": "70d1519c9c754334e016",
+    "url": "/static/js/main.2d5b38c8.chunk.js"
+  },
+  {
+    "revision": "566955bc82714a496ec8",
+    "url": "/static/js/runtime~main.179115f8.js"
+  }
+]);
