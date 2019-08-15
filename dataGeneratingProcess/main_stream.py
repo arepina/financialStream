0acf44b589fa52076a85e8b99bd733d75d071895
Soumya Singh <soumyasingh1@yahoo.com>
2019-08-15 14:13:35 +0100

    added a function to get stream data

--- dataGeneratingProcess/main_stream.py
+++ dataGeneratingProcess/main_stream.py
@@ -16,19 +16,27 @@ def getstream():
         stream = RandomDealData()
         instrumentList = stream.createInstrumentList()
         while True:
+            yield stream.createRandomData(instrumentList)
+
+    for stream in data_stream():
+        print(stream)
+        try:
+            logging.info(stream)
+            requests.post(endpoint.POST_TO, json=stream)  # ERROR
+        except Exception as e:
+            logging.error("Error: ", str(e))
+            print(e)
+            continue
+
+@app.route('/getStreamData')
+def getstreamfrontend():
+     def data_stream():
+            stream = RandomDealData()
+        instrumentList = stream.createInstrumentList()
+        while True:
             yield 'data:{}\n\n'.format(stream.createRandomData(instrumentList))
 
     return Response(data_stream(), mimetype="text/event-stream")
-    # for stream in data_stream():
-    #     print(stream)
-    #     try:
-    #         logging.info(stream)
-    #         requests.post(endpoint.POST_TO, json=stream)  # ERROR
-    #     except Exception as e:
-    #         logging.error("Error: ", str(e))
-    #         print(e)
-    #         continue
-
 
 def bootapp():
     app.run(port=8080, threaded=True, host=(endpoint.HOST), debug=True)
